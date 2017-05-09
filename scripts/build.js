a04ece2ac9fd2cc6c97005e5b1716ae3deaa6a12
Benedikt Rötsch <info@benedikt-roetsch.de>
2017-05-09 14:47:54 +0200

    refactor for single locale files for categories and keywords

--- scripts/build.js
+++ scripts/build.js
@@ -3,48 +3,61 @@ const fsExtra = require('fs-extra')
 const path = require('path')
 const emojis = require('../lib/emojis')
 const ordered = require('../lib/ordered')
+const lib = path.resolve(__dirname, '..', 'lib')
 const dist = path.resolve(__dirname, '..', 'dist')
+const localeRegex = /^locales-(.*)\.json/
 
 // Create a simple map for easier and bandwith saving usage
+console.log('Creating simplemap...')
 const simplemap = Object.keys(emojis).reduce((map, name) => {
   map[name] = emojis[name].char
   return map
 }, {})
 
-// Loop over all emojis and create a copy for every keyword translation they have.
-// This will split them up into separate files with only one language per file.
-const localizedEmojis = Object.keys(emojis).reduce((localizedEmojis, name) => {
-  const emoji = emojis[name]
-  const emojiKeywordLocales = Object.keys(emoji.keywords)
-  emojiKeywordLocales.forEach((emojiLocale) => {
-    // Create a copy which contains keywords only for the given emojiLocale
-    const localizedEmoji = Object.assign({}, emoji, {
-      keywords: emoji.keywords[emojiLocale]
-    })
-
-    if (!localizedEmojis.hasOwnProperty(emojiLocale)) {
-      localizedEmojis[emojiLocale] = {}
-    }
-
-    // And attach it to the fooBar map
-    localizedEmojis[emojiLocale][name] = localizedEmoji
-  })
-
-  return localizedEmojis
+// Find and parse locale files
+console.log('Locating and parsing translation files...')
+const localeMap = fs.readdirSync(lib)
+.filter((fileName) => fileName.match(localeRegex))
+.reduce((files, fileName) => {
+  const matches = localeRegex.exec(fileName)
+  const filePath = path.resolve(lib, fileName)
+  const content = fs.readFileSync(filePath)
+  files[matches[1]] = JSON.parse(content)
+  return files
 }, {})
 
 // Create config data and metadata
 const config = {
   ordered,
   simplemap,
-  locales: Object.keys(localizedEmojis)
+  locales: Object.keys(localeMap)
 }
 
 // Create one emoji file per language
-Object.keys(localizedEmojis).forEach((locale) => {
+Object.keys(localeMap).forEach((locale) => {
   const filename = `emojis-${locale}.json`
-  const json = JSON.stringify(localizedEmojis[locale], null, 2)
-  fs.writeFile(path.resolve(dist, filename), json, (err) => {
+  const localeData = localeMap[locale]
+  const localizedEmojis = Object.keys(emojis).reduce((localizedEmojis, emojiName) => {
+    const emoji = emojis[emojiName]
+    // Translate category when possible, english as fallback
+    if (localeData.categories.hasOwnProperty(emoji.category)) {
+      emoji.category = localeData.categories[emoji.category]
+    }
+
+    // Translate keywords when possible, english as fallback
+    emoji.keywords = emoji.keywords.map((keyword) => {
+      if (localeData.keywords.hasOwnProperty(keyword)) {
+        console.log('translated ' + keyword + ' to ' + localeData.keywords[keyword])
+        return localeData.keywords[keyword]
+      }
+      return keyword
+    })
+    localizedEmojis[emojiName] = emoji
+    return localizedEmojis
+  }, {})
+
+  const localizedJSON = JSON.stringify(localizedEmojis, null, 2)
+  fs.writeFile(path.resolve(dist, filename), localizedJSON, (err) => {
     if (err) {
       console.error(`Failed to write ${filename}`)
       console.error(err)
@@ -52,17 +65,6 @@ Object.keys(localizedEmojis).forEach((locale) => {
     }
     console.log(`Finished writing ${filename}`)
   })
-})
-
-// Write fully localized emojis file
-const localizedJson = JSON.stringify(emojis, null, 2)
-fs.writeFile(path.resolve(dist, 'emojis-localized.json'), localizedJson, (err) => {
-  if (err) {
-    console.error('Failed to write emojis-localized.json')
-    console.error(err)
-    return
-  }
-  console.log('Finished writing emojis-localized.json')
 })
 
 const configJson = JSON.stringify(config, null, 2)
--- scripts/merge-translated-into-new.js
+++ scripts/merge-translated-into-new.js
@@ -14,7 +14,6 @@ const emojis = require('../lib/emojis')
 
 const sourceFilePath = path.resolve(process.argv[2])
 const locale = process.argv[3]
-const distFilePath = path.resolve(__dirname, '..', 'lib', 'emojis.json')
 
 if (!languageTagRegex().test(locale)) {
   console.error('Locale must match the IETF language tag standard. See: https://en.wikipedia.org/wiki/IETF_language_tag')
@@ -26,35 +25,61 @@ if (!sourceFilePath || sourceFilePath.length === 0) {
   process.exit(1)
 }
 
-const newEmojis = require(sourceFilePath)
-
-Object.keys(newEmojis).forEach((name) => {
-  // Add emoji if it does not exist in emojilib
+const distFilePath = path.resolve(__dirname, '..', 'lib', `locales-${locale}.json`)
+const translatedEmojis = require(sourceFilePath)
+const keywordsMap = {}
+const categoryMap = {}
+Object.keys(translatedEmojis).forEach((name) => {
+  // Warn user if emoji does not exist in source of truth emojis.json
   if (!emojis.hasOwnProperty(name)) {
-    emojis[name] = newEmojis[name]
+    console.error(`🚨: Emoji ${name} is not present in emojis.json`)
+    return
   }
 
-  const newKeywords = newEmojis[name].keywords
-  if (Array.isArray(emojis[name].keywords)) {
-    // Transform untranslated keywords into localized
-    emojis[name].keywords = {
-      [locale]: newKeywords
-    }
-  } else {
-    // Or attach keywords to translated keywords
-    if (!emojis.hasOwnProperty(name)) {
-      console.error('Emoji ' + name + ' does not exist in library.')
-      return
-    }
-    emojis[name].keywords[locale] = newKeywords
+  const origEmoji = emojis[name]
+  const translatedEmoji = translatedEmojis[name]
+
+  // Grab translated category
+  categoryMap[origEmoji.category] = translatedEmoji.category
+
+  // Assume that the order of keywords are kept
+  if (origEmoji.keywords.length > translatedEmoji.keywords.length) {
+    console.error(`🚨: Translated emoji ${name} (${origEmoji.char} ) has less keywords as original in emojis.json`)
+    return
   }
+
+  origEmoji.keywords.forEach((keyword, index) => {
+    keywordsMap[keyword] = translatedEmoji.keywords[index]
+  })
 })
 
-fs.writeFile(distFilePath, JSON.stringify(emojis, (value) => {
-  if (Array.isArray(value)) {
-    return JSON.stringify(value, null, 0)
-  }
-  return JSON.stringify(value, null, 2)
-}, 2), () => {
-  console.log('Finished')
+// Sort categories to easy up manual editing
+const sortedCategoryMap = Object.keys(categoryMap).sort().reduce((categories, category) => {
+  return Object.assign(categories, {
+    [category]: categoryMap[category]
+  })
+}, {})
+
+// Sort keywords to easy up manual editing
+const sortedKeywordsMap = Object.keys(keywordsMap).sort().reduce((keywords, keyword) => {
+  return Object.assign(keywords, {
+    [keyword]: keywordsMap[keyword]
+  })
+}, {})
+
+const localizedData = {
+  categories: sortedCategoryMap,
+  keywords: sortedKeywordsMap
+}
+
+// Create JSON with single line keywords array
+const localeJSON = JSON.stringify(localizedData, null, 2)
+.replace(/\[[^\]]*\]/g, (value) => {
+  // remove tabs and line breaks while reducing multiple spaces to a single one
+  return value.replace(/[\t\n\r]+/g, '')
+    .replace(/ +/g, ' ')
+})
+
+fs.writeFile(distFilePath, localeJSON, () => {
+  console.log(`Finished creating lib/locales-${locale}.json`)
 })
