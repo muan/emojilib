575c5de4b38bcdb459440126063d41d6664ab4b4
Svend <svend.jin@qq.com>
2017-10-02 22:14:31 +0800

    :cn: add Chinese keyword

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   lib: require('./emojis'),
+  lib_cn: require('./zh-cn/emojis'),
   ordered: require('./ordered'),
   fitzpatrick_scale_modifiers: ["🏻", "🏼", "🏽", "🏾", "🏿"]
 }
---
+++ zh-cn/converter.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var path = require('path')
+var JSONStream = require('JSONStream')
+var format = require('format-json-stream')
+
+var cnEmojis = fs.createWriteStream(path.join(__dirname, 'emojis.json'))
+
+fs.readFile(path.join(__dirname, 'emoji_words.json'), 'utf8', (err, data) => {
+  if (err) throw err;
+  var emojiCnWords = JSON.parse(data)
+  fs.createReadStream(path.join(__dirname, '../emojis.json'))
+  .pipe(JSONStream.parse('*', function (val, key) {
+    if (val.char) {
+      var words = emojiCnWords[val.char]
+      if (words) {
+        val.keywords.push.apply(val.keywords, words)
+      }
+    }
+    return [key[0], val]
+  }))
+  .pipe(JSONStream.stringifyObject())
+  .pipe(format())
+  .pipe(cnEmojis)
+});
+  
