8532d753191157155c5015779aad86292ada839d
Mu-An ✌️ Chiou <me@muanchiou.com>
2018-11-20 18:28:33 -0500

    Refactor tests There's no need for phantomjs cc @jasonrudolph

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /* global phantom */
 
 var fs = require('fs')
-var rawData = fs.read('emojis.json').toString()
-var buildFailed = false
+var rawData = fs.readFileSync('./emojis.json').toString()
+var test = require('tape')
 var passed = function () { console.log('\x1B[92mPASSED\x1B[0m\n') }
 var failed = function () {
   console.log('\x1B[91mFAILED\x1B[0m\n')
@@ -33,182 +33,131 @@ var categories = {
 var numberOfEmojis = Object.keys(categories).map(function (key) { return categories[key] }).reduce(function (a, b) { return a + b })
 
 //
-console.log('\nTEST: Correct JSON format')
+test('Keys match', function(t) {
+  data = JSON.parse(fs.readFileSync('emojis.json').toString())
+  ordered_keys = JSON.parse(fs.readFileSync('ordered.json').toString())
+  keys = Object.keys(data)
+  t.ok(keys, 'Valid JSON format')
+  t.equal(keys.length, numberOfEmojis, keys.length + '/' + numberOfEmojis +  ' objects in emojis.json')
+  t.equal(ordered_keys.length, numberOfEmojis, ordered_keys.length + '/' + numberOfEmojis + ' keys in ordered_keys.json')
 
-try {
-  var data = JSON.parse(fs.read('emojis.json'))
-  var orderd_keys = JSON.parse(fs.read('ordered.json'))
-  var keys = Object.keys(data)
-  passed()
-} catch (e) {
-  console.log('Invalid JSON format. See the CONTRIBUTING doc for reference.')
-  failed()
-  reveal()
-}
-
-//
-console.log('TEST: Correct number of emojis')
-
-if (keys.length !== numberOfEmojis) {
-  console.log('There are ' + numberOfEmojis + ' emojis, but emojis.json has ' + keys.length + ' entries.')
-  failed()
-} else {
-  passed()
-}
-
-//
-console.log('TEST: Ordered keys are up to date')
-
-ordered_keys_not_in_keys = orderd_keys.filter(function(key) { return keys.indexOf(key) < 0})
-if (orderd_keys.length !== numberOfEmojis) {
-  console.log('There are ' + numberOfEmojis + ' emojis, but keys contains ' + orderd_keys.length + ' emojis.')
-  failed()
-} else if (ordered_keys_not_in_keys.length > 0) {
-  console.log('Failed to find ' + ordered_keys_not_in_keys.join(', ') + ' in emojis.json.')
-  failed()
-} else {
-  passed()
-}
-
-//
-console.log('TEST: Correct number of emojis in each category')
-
-var counter = []
-
-Object.keys(categories).forEach(function (category) {
-  var count = keys.map(function (key) {
-    return data[key]['category']
-  }).filter(function (mojicategory) {
-    return mojicategory === category
-  }).length
-
-  if (count !== categories[category]) {
-    counter.push([category, count])
-  }
+  var ordered_keys_not_in_keys = ordered_keys.filter(function(key) { return keys.indexOf(key) < 0})
+  t.deepEqual(ordered_keys_not_in_keys, [], 'Keys in emojis.json and ordered.json match')
+  t.end()
 })
 
-if (counter.length > 0) {
-  counter.forEach(function (result) {
-    console.log('There are ' + result[1] + ' emojis in \'' + result[0] + '\', but the expected number is ' + categories[result[0]])
-  })
-  failed()
-} else {
-  passed()
-}
+test('Emoji count', function(t) {
+  var counter = []
+  Object.keys(categories).forEach(function (category) {
+    var count = keys.map(function (key) {
+      return data[key]['category']
+    }).filter(function (mojicategory) {
+      return mojicategory === category
+    }).length
 
-//
-console.log('TEST: No duplicated entries')
-
-var arr = []
-var dups = []
-var keysFromRawData = rawData.match(/\".+\": {/g)
-
-keysFromRawData.forEach(function (key) {
-  key = key.replace(/\"|\:|\s|\{/g, '')
-  if (arr.indexOf(key) < 0) {
-    arr.push(key)
-  } else {
-    dups.push(key)
-  }
-})
-
-var charsFromRawData = rawData.match(/"char": ".+"/g)
-
-charsFromRawData.forEach(function (character) {
-  character = character.replace(/:|"|(char)|\s/g, '')
-  if (arr.indexOf(character) < 0) {
-    arr.push(character)
-  } else {
-    dups.push(character)
-  }
-})
-
-if (dups.length > 0) {
-  dups.forEach(function (key) {
-    console.log('There is more than one "' + key + '" in emojis.json.')
-  })
-  failed()
-} else {
-  passed()
-}
-
-//
-console.log('TEST: No unnecessary keywords')
-
-var unnecessities = []
-var unnecessitiesInKeywords = []
-
-keys.forEach(function (key) {
-  data[key]['keywords'].forEach(function (keyword) {
-    if (key === keyword) {
-      unnecessities.push([key, keyword])
+    if (count !== categories[category]) {
+      counter.push([category, count])
     }
+  })
 
-    var otherKeywords = data[key]['keywords'].slice()
-    otherKeywords.splice(data[key]['keywords'].indexOf(keyword), 1)
+  if (counter.length > 0) {
+    var emptyCounterMessage = ''
+    for (const result of counter) {
+      emptyCounterMessage += 'There are ' + result[1] + ' emojis in \'' + result[0] + '\', but the expected number is ' + categories[result[0]] + '\n'
+    }
+  }
+  t.deepEqual(counter, [], emptyCounterMessage || 'OK')
+  t.end()
+})
 
-    otherKeywords.forEach(function (otherKeyword) {
-      if (otherKeyword === keyword) {
-        unnecessitiesInKeywords.push([otherKeyword, keyword, key])
+test('No duplicate characters', function(t) {
+  var arr = []
+  var dups = []
+  var keysFromRawData = rawData.match(/\".+\": {/g)
+
+  keysFromRawData.forEach(function (key) {
+    key = key.replace(/\"|\:|\s|\{/g, '')
+    if (arr.indexOf(key) < 0) {
+      arr.push(key)
+    } else {
+      dups.push(key)
+    }
+  })
+
+  var charsFromRawData = rawData.match(/"char": ".+"/g)
+
+  charsFromRawData.forEach(function (character) {
+    character = character.replace(/:|"|(char)|\s/g, '')
+    if (arr.indexOf(character) < 0) {
+      arr.push(character)
+    } else {
+      dups.push(character)
+    }
+  })
+
+  if (dups.length > 0) {
+    var noDupsMessage = ''
+    for (const key of dups) {
+      noDupsMessage += 'There is more than one "' + key + '" in emojis.json.\n'
+    }
+  }
+  t.deepEqual(dups, [], noDupsMessage || 'OK')
+  t.end()
+})
+
+//
+test('No unnecessary keywords', function(t) {
+  var unnecessities = []
+  var unnecessitiesInKeywords = []
+
+  keys.forEach(function (key) {
+    for (const keyword of data[key]['keywords']) {
+      if (key === keyword) {
+        unnecessities.push([key, keyword])
       }
-    })
+
+      var otherKeywords = data[key]['keywords'].slice()
+      otherKeywords.splice(data[key]['keywords'].indexOf(keyword), 1)
+
+      otherKeywords.forEach(function (otherKeyword) {
+        if (otherKeyword === keyword) {
+          unnecessitiesInKeywords.push([otherKeyword, keyword, key])
+        }
+      })
+    }
   })
+
+  if (unnecessities.length > 0 || unnecessitiesInKeywords.length > 0) {
+    var unnecessitiesMessage = ''
+    for (const arr of unnecessities) {
+      unnecessitiesMessage += '"' + arr[1] + '" is unnecessary as the key is already "' + arr[0] + '".\n'
+    }
+    var unnecessitiesInKeywordsMessage = ''
+    for (const arr of unnecessitiesInKeywords) {
+      unnecessitiesInKeywordsMessage += '"' + arr[1] + '" is unnecessary as a "' + arr[2] + '" already has a keyword "' + arr[0] + '".\n'
+    }
+  }
+  t.deepEqual(unnecessities, [], unnecessitiesMessage || 'OK')
+  t.deepEqual(unnecessitiesInKeywords, [], unnecessitiesInKeywordsMessage || 'OK')
+  t.end()
 })
-
-if (unnecessities.length > 0 || unnecessitiesInKeywords.length > 0) {
-  unnecessities.forEach(function (arr) {
-    console.log('"' + arr[1] + '" is unnecessary as the key is already "' + arr[0] + '".')
-  })
-  unnecessitiesInKeywords.forEach(function (arr) {
-    console.log('"' + arr[1] + '" is unnecessary as a "' + arr[2] + '" already has a keyword "' + arr[0] + '".')
-  })
-
-  failed()
-} else {
-  passed()
-}
 
 //
-console.log('TEST: Line format')
-
-var offenses = []
-var lines = rawData.replace(/^{\n([\s\S]*)\n}\n$/, '$1').split('\n')
-var baseRegex = '^    "keywords": \\["[^\"]+"(, "[^\"]+")*\\]'
-var contentRegex = new RegExp(baseRegex + ',$')
-lines.forEach(function (line, index) {
-  if (line.match(/keywords/) && !line.match(contentRegex)) {
-    offenses.push(index + 2)
-  }
-})
-
-if (offenses.length > 0) {
-  offenses.forEach(function (lineNo) {
-    console.log('Line ' + lineNo + ' has the wrong format.')
+test('Required properties', function(t) {
+  var properties = ['keywords', 'char', 'fitzpatrick_scale', 'category']
+  var emojiWithWrongKeys = []
+  keys.forEach(function (key) {
+    if (Object.keys(data[key]).join() !== properties.join()) {
+      emojiWithWrongKeys.push(key)
+    }
   })
 
-  failed()
-} else {
-  passed()
-}
-
-//
-console.log('TEST: Properties')
-
-var properties = ["keywords", "char", "fitzpatrick_scale", "category"]
-var emojiWithWrongKeys = []
-keys.forEach(function (key) {
-  if (Object.keys(data[key]).join() !== properties.join()) {
-    emojiWithWrongKeys.push(key)
+  if (emojiWithWrongKeys.length > 0) {
+    var message = ''
+    for (const emoji of emojiWithWrongKeys) {
+      message += emoji + ' has keys "' + Object.keys(data[emoji]).join(', ') + '", expect: "' + properties.join(', ') + '"\n'
+    }
   }
+  t.deepEqual(emojiWithWrongKeys, [], message || 'OK')
+  t.end()
 })
-if (emojiWithWrongKeys.length > 0) {
-  emojiWithWrongKeys.forEach(function (emoji) {
-    console.log(emoji + ' has keys "' + Object.keys(data[emoji]).join(', ') + '", expect: "' + properties.join(', ') + '"')
-  })
-
-  failed()
-} else {
-  passed()
-}
-
-reveal()
